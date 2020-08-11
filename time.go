24497c67c376367c9044ee18d23ad3c11086c5ff
airplayx <airplayx@gmail.com>
2020-08-11 13:54:13 +0800

    update

--- time.go
+++ time.go
@@ -1,6 +1,41 @@
 package gorbit
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
+
+type MyTime time.Time
+
+func (mt *MyTime) UnmarshalJSON(bs []byte) error {
+	var s string
+	err := json.Unmarshal(bs, &s)
+	if err != nil {
+		return err
+	}
+	t, err := time.ParseInLocation("2006-01-02 15:04:05", s, time.Local)
+	if err != nil {
+		return err
+	}
+	*mt = MyTime(t)
+	return nil
+}
+
+func (mt *MyTime) MarshalJSON() ([]byte, error) {
+	b := make([]byte, 0, len("2006-01-02 15:04:05")+2)
+	b = append(b, '"')
+	b = time.Time(*mt).AppendFormat(b, "2006-01-02 15:04:05")
+	b = append(b, '"')
+	return b, nil
+}
+
+func (mt *MyTime) Time() time.Time {
+	return time.Time(*mt)
+}
+
+func (mt *MyTime) String() string {
+	return time.Time(*mt).Format("2006-01-02 15:04:05")
+}
 
 func Day0(diffDay int) time.Time {
 	t := time.Now()
