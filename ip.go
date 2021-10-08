7f7a40c137a80fef507f81fb5b62e4075377e1ad
airplayx <airplayx@gmail.com>
2021-10-08 10:10:15 +0800

    add `LocalIPv4s`

--- ip.go
+++ ip.go
@@ -26,3 +26,18 @@ func Long2ip(ip uint32) (net.IP, error) {
 	}
 	return net.IPv4(a, b, c, d), nil
 }
+
+func LocalIPv4s() ([]string, error) {
+	var ips []string
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ips, err
+	}
+
+	for _, a := range addrs {
+		if ipNet, ok := a.(*net.IPNet); ok && !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
+			ips = append(ips, ipNet.IP.String())
+		}
+	}
+	return ips, nil
+}
