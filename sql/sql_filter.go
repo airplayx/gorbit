7e09cceef1369e064e9ca7d96365d1776bb14a3b
airplayx <airplayx@gmail.com>
2023-10-12 13:57:46 +0800

    update `SafeString` func

--- sql/sql_filter.go
+++ sql/sql_filter.go
@@ -1,25 +1,18 @@
 package sql
 
 import (
-	"log"
 	"regexp"
 	"strings"
 )
 
-var reStr = `(?:')|(?:\%)|(?:\\)|(?:--)|(/\\*(?:.|[\\n\\r])*?\\*/)|(\b(select|update|and|or|delete|insert|trancate|char|chr|into|substr|ascii|declare|exec|count|master|into|drop|execute)\b)`
+const unsafeKeys = `(?:')|(?:\%)|(?:\\)|(?:--)|(/\\*(?:.|[\\n\\r])*?\\*/)|(\b(select|update|and|or|delete|insert|trancate|char|chr|into|substr|ascii|declare|exec|count|master|into|drop|execute)\b)`
 
 func SafeString(matchStr string, exactly bool) string {
-	re, err := regexp.Compile(reStr)
-	if err != nil {
-		log.Println(err)
-		return ``
-	}
-	return re.ReplaceAllStringFunc(matchStr, func(dst string) string {
+	ruler := regexp.MustCompile(unsafeKeys)
+	return ruler.ReplaceAllStringFunc(matchStr, func(dst string) string {
 		if dst == `\` && !exactly {
-			dst = strings.ReplaceAll(dst, dst, `\\\`+dst)
-		} else {
-			dst = strings.ReplaceAll(dst, dst, `\`+dst)
+			return strings.ReplaceAll(dst, dst, `\\\`+dst)
 		}
-		return dst
+		return strings.ReplaceAll(dst, dst, `\`+dst)
 	})
 }
